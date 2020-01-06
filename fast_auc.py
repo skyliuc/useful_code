4bed532f55bda587993896479757f51e024b8d37
HangChengLiu <32815226+skyliuc@users.noreply.github.com>
2020-01-06 14:48:36 +0800

    Create fast_auc.py
    fast_auc

---
+++ fast_auc.py
@@ -0,0 +1,15 @@
+def fast_auc(y_true, y_prob):
+    """
+    fast roc_auc computation: https://www.kaggle.com/c/microsoft-malware-prediction/discussion/76013
+    """
+    y_true = np.asarray(y_true)
+    y_true = y_true[np.argsort(y_prob)]
+    nfalse = 0
+    auc = 0
+    n = len(y_true)
+    for i in range(n):
+        y_i = y_true[i]
+        nfalse += (1 - y_i)
+        auc += y_i * nfalse
+    auc /= (nfalse * (n - nfalse))
+    return auc
